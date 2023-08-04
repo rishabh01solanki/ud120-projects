7d1c63a121ad36a2fe204b82482120bf3809237f
rishabh01solanki <rishabh01solanki@gmail.com>
2023-08-04 16:45:29 -0500

    updated svm and naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -8,12 +8,11 @@
     authors and labels:
     Sara has label 0
     Chris has label 1
-"""
-    
+"""    
 import sys
-from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from time import time
 
 
 ### features_train and features_test are the features for the training
@@ -24,9 +23,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+t0 = time() # start timer for training
+clf.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s") # time taken for training
 
+t0 = time () # prediction time
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s") # time taken for prediction
 
+from sklearn.metrics import accuracy_score
+result = accuracy_score(labels_test, pred)
+print(result)
 ##############################################################
 
 ##############################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,12 +7,11 @@
     Sara has label 0
     Chris has label 1
 """
-    
+   
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,8 +19,30 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
+
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+
+clf = svm.SVC(kernel='rbf', C=10000)
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s") # time taken for training
+
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s") # time taken for predicting
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels_test,pred))
+
+import numpy as np
+print(np.count_nonzero(pred == 1))
+
+
 
 
 #########################################################
